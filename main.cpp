907e5119bf790f2dcb28c351a171a820cd9369e4
Repnikov Pavel <prepnik@mail.ru>
2022-07-25 22:26:01 +0300

    start SQLiteTest branch

--- main.cpp
+++ main.cpp
@@ -1,61 +1,17 @@
-﻿// master branch
+﻿// TestSQLite branch
 
 #include <string>
 #include <iostream>
 #include <format>
-#include <uwebsockets/App.h>
 #include "DataStructures.h"
 #include "GlobalVariables.h"
 #include "Serv.h"
 
+#include "sqlite/sqlite3.h"
+
 int main() {
-    
-    //"ws://localhost:8080" 
-    //"/*"
-    std::vector<std::thread*> Threads(std::thread::hardware_concurrency());
-    std::transform(Threads.begin(), Threads.end(), Threads.begin(),
-        [](auto* thr)
-        {
-            return new std::thread(
-                []()
-                {
-                    uWS::App().ws<UserFromUEConnectionInfo>
-                        (
-                            "/*",
-                            {
-                                .open = [](auto* ws)
-                                {
-                                    UserFromUEConnectionInfo* data = ws->getUserData();
-                                    data->user_id = gv::latest_user_id++;
 
-                                },
-                                .message = [](auto* ws, std::string_view message,uWS::OpCode OpCode)
-                                {
+    sqlite3* DB;
 
-
-                                }
-
-
-                            }
-                    ).listen(8080, [](auto* token)
-                        {
-                            if (token)
-                            {
-                                std::cout << std::format("server start and listening on port {}\n", 8080);
-                            }
-                            else
-                            {
-                                std::cout << std::format("server failed to start");
-
-                            }
-
-                        }
-                    ).run();
-
-                }
-            );
-        }
-    );
-
-    std::for_each(Threads.begin(), Threads.end(), [](auto* thr) {thr->join(); });
+    return 0;
 }
