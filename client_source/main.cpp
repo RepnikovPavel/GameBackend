c09f0b499f69171b4b939283184536dd23c4063b
Repnikov Pavel <prepnik@mail.ru>
2022-09-19 20:28:14 +0300

    start using boost

---
+++ client_source/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <format>
+#include <vector>
+
+int main()
+{
+	
+	std::vector<double> vec_x(10);
+	std::vector<double> vec_y(10);
+	//system("server.exe plot(x,y)"+" x:"to_str(vec_x) +" y:"+to_str(vec_y))
+	return 0;
+}
---
+++ server_source/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <format>
+#include <vector>
+#include <boost/beast.hpp>
+
+int main()
+{
+	
+	std::vector<double> vec_x(10);
+	std::vector<double> vec_y(10);
+	
+
+	return 0;
+}
